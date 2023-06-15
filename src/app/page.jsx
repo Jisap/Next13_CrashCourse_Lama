e9dd977143e895c96559ae3a88e0f8cb229632df
Jorge Matos <jisapjisap@gmail.com>
2023-06-15 16:06:24 +0200

    home & about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/hero.png'
+import Button from '@/components/Button/Button'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design gor your digital product.</h1>
+        <p className={styles.desc}>Turning your Idea into Reality. We brin together the teams from the global tech industry.</p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.item}>
+        <Image src={ Hero } alt="" className={styles.img}/>
+      </div>
     </div>
   )
 }
